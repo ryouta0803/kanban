1ed151418190c88887d0d9ceb8d48d3d97a2d4f2
ryouta0803 <67770359+ryouta0803@users.noreply.github.com>
2020-07-27 16:55:40 +0900

    ユーザーをリンクにしたくて試行錯誤

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'top#index'
-  resources :user, only: %i(edit update)
-  resources :list, only: %i(new create edit update destroy) do
+    resources :users, only: %i(:edit, :update)
+    resources :list, only: %i(new create edit update destroy) do
     resources :card, except: %i(index)
   end
 end
